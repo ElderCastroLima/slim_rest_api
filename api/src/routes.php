1aea9616d1acf4b0f29d7925ba0901a731600443
eldercastrolima <eldercastro.desenvolvedor@gmail.com>
2017-04-02 16:58:04 -0400

    testando a conexão com mysql

--- api/src/routes.php
+++ api/src/routes.php
@@ -1,12 +1,28 @@
 <?php
 // Routes
 
-$app->get('/teste', function ($request, $response, $args) {
+$app->get('/getAllGuitars', function ($request, $response, $args) {
     // Sample log message
-    $this->logger->info("Teste '/' route");
+    $this->logger->info("getAllGuitars '/' route");
+
+    $sth = $this->db->prepare("SELECT * FROM GUITARS"); 
+    $sth->execute();
+    $result = $sth->fetchAll();
+    return $this->response->withJson($result);
 
     // Render index view
-    return $this->renderer->render($response, 'route2.phtml', $args);
+    //return $this->renderer->render($response, 'route2.phtml', $args);
+});
+
+$app->get('/getGuitarsById/[{id}]', function ($request, $response, $args) {
+   //Sample log getGuitarById
+	$this->logger->info("getGuitarById '/' route");
+
+	$sth = $this->db->prepare("SELECT * FROM GUITARS WHERE FK_SERIES = :id");
+	$sth->bindParam("id",$args['id']);
+	$sth->execute();
+	$result = $sth->fetchObject();
+	return $this->response->withJson($result);
 });
 
 $app->get('/[{name}]', function ($request, $response, $args) {
