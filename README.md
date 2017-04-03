d140a58c545fb5c4eacb0296d9b6e5b575af11c0
eldercastro <eldercastro.desenvolvedor@gmail.com>
2017-04-03 15:30:11 -0400

    Finalizando os metodos get, post, put e delete

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # slim_rest_api
 Criando uma api rest com Slim framework
+
+Projeto para estudo do framework Slim 
+
+Referências : 
+
+https://arjunphp.com/creating-restful-api-slim-framework/
+
+http://expalmer.github.io/criando-um-rest-com-slim-framework-e-illuminate-database-em-poucos-minutos-parte-1/
+
+Usei esses dois links e a documentação oficial do Slim como referencia de estudo e após entender criei essa api para fins de estudo.
--- api/src/routes.php
+++ api/src/routes.php
@@ -1,13 +1,13 @@
 <?php
 // Routes
-
+// GET ------------------------------------------------------------
 $app->get('/getAllGuitars', function ($request, $response, $args) {
     // Sample log message
-    $this->logger->info("getAllGuitars '/' route");
+    $this->logger->info("getAllGuitars");
 
-    $sth = $this->db->prepare("SELECT * FROM GUITARS"); 
-    $sth->execute();
-    $result = $sth->fetchAll();
+    $stmt = $this->db->prepare("SELECT * FROM GUITARS"); 
+    $stmt->execute();
+    $result = $stmt->fetchAll();
     return $this->response->withJson($result);
 
     // Render index view
@@ -16,12 +16,12 @@ $app->get('/getAllGuitars', function ($request, $response, $args) {
 
 $app->get('/getGuitarsById/[{id}]', function ($request, $response, $args) {
    //Sample log getGuitarById
-	$this->logger->info("getGuitarById '/' route");
+	$this->logger->info("getGuitarById");
 
-	$sth = $this->db->prepare("SELECT * FROM GUITARS WHERE FK_SERIES = :id");
-	$sth->bindParam("id",$args['id']);
-	$sth->execute();
-	$result = $sth->fetchObject();
+	$stmt = $this->db->prepare("SELECT * FROM GUITARS WHERE id = :id");
+	$stmt->bindParam("id",$args['id']);
+	$stmt->execute();
+	$result = $stmt->fetchObject();
 	return $this->response->withJson($result);
 });
 
@@ -31,4 +31,70 @@ $app->get('/[{name}]', function ($request, $response, $args) {
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+// POST --------------------------------------------------
+$app->post('/insertGuitars',function($request, $response){
+    //Sample log InsertGuitars
+    $this->logger->info("InsertGuitars");
+    //get data body request
+    $input = $request->getParsedBody();
+    //Sample log data
+    $this->logger->info("InsertGuitars param:" .$input['id']
+                                       .', '.$input['fk_brands']
+                                       .', '.$input['fk_series']
+                                       .', '.$input['description']
+                                       .', '.$input['created_at']
+                                       .', '.$input['updated_at']
+                                      );
+    $sql = "INSERT INTO GUITARS (ID, FK_BRANDS, FK_SERIES, DESCRIPTION, CREATED_AT, UPDATED_AT) 
+                   VALUES (:id,:fk_brands,:fk_series,:description,:created_at,:updated_at)";
+    $stmt = $this->db->prepare($sql);
+    $stmt->bindParam(":id",$input['id']);
+    $stmt->bindParam(":fk_brands",$input['fk_brands']);
+    $stmt->bindParam(":fk_series",$input['fk_series']);
+    $stmt->bindParam(":description",$input['description']);
+    $stmt->bindParam(":created_at",$input['created_at']);
+    $stmt->bindParam(":updated_at",$input['updated_at']);
+    $stmt->execute();
+
+    //Retornar alguma mensagem?   
+});
+
+// DELETE---------------------------------------------------------------------
+$app->delete('/deleteGuitarsById/[{id}]',function($request, $response, $args){ 
+    //Sample log DeleteGuitarsById
+    $this->logger->info("deleteGuitarsById");
+
+   $stmt = $this->db->prepare("DELETE FROM GUITARS WHERE id = :id");
+   $stmt->bindParam("id",$args["id"]);
+   $stmt->execute();
+
+});
+
+// PUT---------------------------------------------------------------------
+$app->put('/updateGuitarsById/[{id}]',function($request, $response, $args){
+  $this->logger->info("updateGuitarsById Código =>".$args["id"]);
+  //get data body request 
+  $input = $request->getParsedBody(); 
+
+  $sql = "UPDATE GUITARS SET FK_BRANDS = :fk_brands, 
+                             FK_SERIES = :fk_series, 
+                             DESCRIPTION = :description, 
+                             UPDATED_AT = :updated_at
+                         WHERE ID = :id";
+  //Sample log data
+  $this->logger->info("updateGuitarsById param:" .$input['fk_brands']
+                                         .', '.$input['fk_series']
+                                         .', '.$input['description']
+                                         .', '.$input['created_at']
+                                         .', '.$input['updated_at']
+                                         );
+  $stmt = $this->db->prepare($sql);
+  $stmt->bindParam(":id",$args["id"]);
+  $stmt->bindParam(":fk_brands",$input['fk_brands']);
+  $stmt->bindParam(":fk_series",$input['fk_series']);
+  $stmt->bindParam(":description",$input['description']);
+  $stmt->bindParam(":updated_at",$input['updated_at']);
+  $stmt->execute();
 });
